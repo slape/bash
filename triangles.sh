c5653cfb11d117dd9d63371e505c1ab2343e509e
slape <isaacslape@gmail.com>
2018-06-03 15:52:53 -0400

    triangles

---
+++ triangles.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#Determine a type of triangle.
+
+echo Enter the integer of the first side:
+read x
+echo Enter the integer of the second side:
+read y
+echo Enter the integer of the third side:
+read z
+
+if (( $x == $y && $x == z )); then
+echo ''
+echo This is an equilateral triangle.
+echo ''
+elif (( $x != $y && $x != $z && $y != $z )); then
+echo ''
+echo This is a scalene triangle.
+echo ''
+elif (( $x != $y && $x == $z || $x != $z && $x == $y )); then
+echo ''
+echo This is an isosceles triangle.
+echo ''
+fi
