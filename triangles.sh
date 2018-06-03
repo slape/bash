bbd56cb156400337c1709c146613c0a5720234e7
slape <isaacslape@gmail.com>
2018-06-03 16:35:28 -0400

    indentation

--- triangles.sh
+++ triangles.sh
@@ -9,15 +9,15 @@ echo Enter the integer of the third side:
 read z
 
 if (( $x == $y && $x == z )); then
-echo ''
-echo This is an equilateral triangle.
-echo ''
+  echo ''
+  echo This is an equilateral triangle.
+  echo ''
 elif (( $x != $y && $x != $z && $y != $z )); then
-echo ''
-echo This is a scalene triangle.
-echo ''
+  echo ''
+  echo This is a scalene triangle.
+  echo ''
 elif (( $x != $y && $x == $z || $x != $z && $x == $y || $z == $y && $y != $x )); then
-echo ''
-echo This is an isosceles triangle.
-echo ''
+  echo ''
+  echo This is an isosceles triangle.
+  echo ''
 fi
