c96d1cb52030ea7b6d309f913d1901563979eac0
slape <isaacslape@gmail.com>
2018-06-03 17:54:18 -0400

    loops

---
+++ bashLoops.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+
+#counting numbers with a for loop.
+#Bash allows two different syntaxes.
+
+#lists numbes 0 - 5
+for (( i = 0; i < 6; i++ )); do
+  echo $i
+done
+
+echo ""
+
+#lists numbes 0 - 5
+for i in {0..5}; do
+  echo $i
+done
+
+echo ""
+
+#loops through the files in the directory and applies an action (ls in this case)
+dirlist=`ls`
+for i in $dirlist; do
+  ls $i
+done
+
+echo ""
+
+#while loops
+
+#list the numbers 0 - 5
+x=0
+while [[ $x -le 5 ]]; do
+  echo $x
+  let x+=1
+done
+
+echo ""
+
+#loop through all the files in a directory and print files owned by root.
+ls -la | while read line; do
+  fileName=`echo $line | awk '{print $9}'`
+  owner=`echo $line | awk '{print $3}'`
+  if [[ $owner == "root" ]]; then
+    echo The file $fileName is owned by root.
+  fi
+done
