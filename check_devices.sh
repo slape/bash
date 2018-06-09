18d94ac884aa24158972a384851b3026b726a3c9
slape <isaacslape@gmail.com>
2018-06-08 22:29:25 -0400

    script to check if devices have gone offline

--- check_devices.sh
+++ check_devices.sh
@@ -1,4 +1,4 @@
-#A script to check if one or more devices is offline.
+#A script to check if one or more devices is offline. jq is required.
 #If a deivce is offline this script sends an email notification.
 #Schedule this script to run at whatever interval you would like using a cron job.
 
@@ -20,5 +20,5 @@ currentDevices=`curl -u $user $restUrl/Device?availability=Online | jq '.[].name
 if (( $currentDevices == 0 )); then
   mail -s  "$server may be down or offline" $email <<< "The curl command for $server is not returning any data. Something is wrong."
 elif  (( $deviceCount != $currentDevices )); then
-  mail -s  "$server has a device offline" $email <<< "One or more devices has gone offline for QTS server $server"
+  mail -s  "$server has a device offline" $email <<< "One or more devices has gone offline server $server"
 fi
