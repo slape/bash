245926e0f776bed88388328445d74af5c6292b84
Isaac Slape <github@ajnin.33mail.com>
2019-10-08 14:37:50 -0400

    Cleaned up / reorganized repo

---
+++ practice/sh.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+name='jake'
+echo "Hello $name."
+echo -e "\nThis is my script.\n"
+echo -e "The files in $PWD are: \n$(ls)"
+echo -e "\nCopying the passwd file to your current directory.\n"
+cp /etc/passwd $PWD
+echo -e "The files in $PWD are now: \n$(ls)"
+echo " "
+echo "Have a great day!"
